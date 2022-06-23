3fe0a4e62490a8ae21a66973987a004589e18e26
Syed Refaie <syedmrefaie@gmail.com>
2022-06-23 09:25:02 -0700

    Add basic math functions, and skeleton files

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+function add(num1, num2) {
+    result = num1+num2;
+    return result;
+}
+
+function subtract(num1, num2) {
+    result = num1 - num2;
+    return result;
+}
+
+function multiply(num1, num2) {
+    result = num1*num2;
+    return result;
+}
+
+function divide(num1, num2) {
+    if (num2 == 0) {
+        console.log('what are you doing lad')
+    }
+    result = num1/num2;
+    return result;
+}
