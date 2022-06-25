98a56242084f720f267b63e01fec430b20c901aa
Syed Refaie <syedmrefaie@gmail.com>
2022-06-25 10:15:01 -0700

    Add and order Buttons, update results box
    Added all the buttons that will be utilized in the calculator,
    with no CSS styling aside from flex properties to make the
    buttons remain in the place where they should. The results display
    also now updates with the ID of the button last pressed.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 
+const buttons = document.querySelectorAll('button')
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            display(button.id);
+        });
+    });
+
 function add(num1, num2) {
     result = num1+num2;
     return result;
@@ -21,3 +28,33 @@ function divide(num1, num2) {
     result = num1/num2;
     return result;
 }
+
+function operate(op, num1, num2) {
+    if (op == '+') {
+        add(num1, num2);
+    }
+    else if (op == '-') {
+        subtract(num1, num2);
+    }
+    else if (op == '*') {
+        multiply(num1,num2);
+    }
+    else if (op == '/') {
+        divide(num1, num2);
+    }
+    return result;
+}
+
+function display(data) {
+    const results = document.querySelector("#results")
+    results.textContent = `${data}`;
+}
+
+/* function buttonPress() {
+    const buttons = document.querySelectorAll('button')
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            alert(button.id);
+        });
+    });
+} */
